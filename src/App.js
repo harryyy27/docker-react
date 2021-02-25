dd2a14b84ffadfbf09826babb46eb9744986b0a2
Harry Mead <harryyy27@gmail.com>
2021-02-25 19:07:02 +0000

    added travis yml

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn to REACT!!!!!
+          Learn react
         </a>
       </header>
     </div>
